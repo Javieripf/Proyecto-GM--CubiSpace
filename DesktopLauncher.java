ea1dbeab3e22c7d13f04786e9db908f6c25d9990
Javieripf <javier.poblete.f@mail.pucv.cl>
2025-11-21 15:58:11 -0300

    Add files via upload

--- DesktopLauncher.java
+++ DesktopLauncher.java
@@ -2,14 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.JuegoProgra;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
-		new Lwjgl3Application(new JuegoProgra(), config);
-	}
+    public static void main (String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setTitle("My GDX Game");
+
+        // GM2.1 – Uso del Singleton JuegoProgra
+        new Lwjgl3Application(JuegoProgra.getInstancia(), config);
+    }
 }
